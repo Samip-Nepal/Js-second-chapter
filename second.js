ea9e0a238a3751709d2dbc7a36e995d1ffe39ef6
Samip-Nepal <samipnepal5@email.com>
2024-01-29 23:00:32 +0545

    Adding increment and decrement operator

--- second.js
+++ second.js
@@ -2,12 +2,24 @@ let a=8;
 let b=5;
 // Arithmetic operator
 console.log("a=" ,a ,"b=",b);
-console.log("a+b=",a + b);
-console.log("a-b=",a - b);
-console.log("a*b=",a * b);
-console.log("a/b=",a / b);
-// Extra operator 
-console.log("a%b=",a % b);
-console.log("a**b=",a ** b);
+// console.log("a+b=",a + b);
+// console.log("a-b=",a - b);
+// console.log("a*b=",a * b);
+// console.log("a/b=",a / b);
+// // Extra operator 
+// console.log("a%b=",a % b);
+// console.log("a**b=",a ** b);
+//Increment and decrement operator
+// a++;
+// console.log("a++ =",a);
+// b--;
+// console.log("b-- =",b);
+// pre and post increment and decrement 
+console.log("a=",a++);
+console.log(a);
+console.log("a=",--a);
+console.log(a);
+
+
 
 
