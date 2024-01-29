90a0833c4c8e1cbdcaefb4f84db39ad3da9d4a74
Samip-Nepal <samipnepal5@email.com>
2024-01-29 22:41:24 +0545

    Adding arithmetic operator with modulus and exponentiation

---
+++ second.js
@@ -0,0 +1,13 @@
+let a=8;
+let b=5;
+// Arithmetic operator
+console.log("a=" ,a ,"b=",b);
+console.log("a+b=",a + b);
+console.log("a-b=",a - b);
+console.log("a*b=",a * b);
+console.log("a/b=",a / b);
+// Extra operator 
+console.log("a%b=",a % b);
+console.log("a**b=",a ** b);
+
+
