8265bbb973a1e77938693871be2eeff714b561b0
Samip-Nepal <samipnepal5@email.com>
2024-01-30 23:26:27 +0545

    Adding assigment operator

--- second.js
+++ second.js
@@ -11,14 +11,31 @@ console.log("a=" ,a ,"b=",b);
 // console.log("a**b=",a ** b);
 //Increment and decrement operator
 // a++;
-// console.log("a++ =",a);
-// b--;
-// console.log("b-- =",b);
-// pre and post increment and decrement 
-console.log("a=",a++);
-console.log(a);
-console.log("a=",--a);
-console.log(a);
+//  console.log("a++ =",a);
+//  b--;
+//  console.log("b-- =",b);
+//pre and post increment and decrement 
+//  console.log("a=",a++);
+// console.log(a);
+// console.log("a=",--a);
+//  console.log(a);
+ //Assigment operator
+ a+=1;
+ console.log("a=",a);
+ a-=1;
+ console.log("a=",a);
+ a*=1;
+ console.log("a=",a);
+ b/=1;
+ console.log("b=",b);
+ b%=3;
+ console.log("b=",b);
+ b**=2;
+ console.log("b=",b);
+
+
+
+
 
 
 
