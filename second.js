b1e8cd11f7d6c1d40a8dc8d22345b5325021b47e
Samip-Nepal <samipnepal5@email.com>
2024-02-03 23:40:35 +0545

    Adding if statement

--- second.js
+++ second.js
@@ -1,54 +1,13 @@
-let a=8;
-let b=5;
-
-console.log("a=" ,a ,"b=",b);
-//logical operator
-console.log(a==8 && a>b);//true And operator
-console.log(a==8 && a<b);//False
-console.log(a==8 || a<b);//true Or operator
-console.log(a==5 || a<b);//False
-console.log(!(a==5));//True
-
-//Arthmentic operator
-// console.log("a+b=",a + b);
-// console.log("a-b=",a - b);
-// console.log("a*b=",a * b);
-// console.log("a/b=",a / b);
-// // Extra operator 
-// console.log("a%b=",a % b);
-// console.log("a**b=",a ** b);
-//Increment and decrement operator
-// a++;
-//  console.log("a++ =",a);
-//  b--;
-//  console.log("b-- =",b);
-//pre and post increment and decrement 
-//  console.log("a=",a++);
-// console.log(a);
-// console.log("a=",--a);
-//  console.log(a);
- //Assigment operator
-//  a+=1;
-//  console.log("a=",a);
-//  a-=1;
-//  console.log("a=",a);
-//  a*=1;
-//  console.log("a=",a);
-//  b/=1;
-//  console.log("b=",b);
-//  b%=3;
-//  console.log("b=",b);
-//  b**=2;
-//  console.log("b=",b);
-//Comparison operator
-// console.log(a==b);//false
-// console.log(a!==b);//true
-// console.log(a===b);//false(its check string also)
-// console.log(a!=b);//true
-// console.log(a>b);//true
-// console.log(a<b);//false
-// console.log(a>=b);//true
-// console.log(a<=b);//false
+//Condition statement
+let mode="light";
+let color;
+if(mode==="light"){
+    color="DarK";
+}
+if(mode==="dark"){
+    color="Green";
+}
+console.log(color);
 
 
 
