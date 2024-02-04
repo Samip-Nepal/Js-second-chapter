4c527b6a07d83ef74db88c31d0a3c8a4a00bd353
Samip-Nepal <samipnepal5@email.com>
2024-02-04 10:49:26 +0545

    Simple switch statement

--- second.js
+++ second.js
@@ -1,5 +1,24 @@
 //Condition statement
-
+let exp="Samip";
+switch (exp) {
+    case "Nepal":
+        {
+            console.log("Samip is god boy");
+        }
+        
+        break;
+        case "Student":
+            {
+                console.log("Samip hero");
+                
+            }
+break;
+case "Samip": {
+console.log("Don");
+}
+    default:"Ok"
+        break;
+}
 
 
 
