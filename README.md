171dca38ec0193a8a8a440c42f031f09177c4bec
Samip-Nepal <samipnepal5@email.com>
2024-02-04 11:16:33 +0545

    Pratice qn 2

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Js-second-chapter
+Pratice qn 
+1)Get user to input a number using prompt("Enter the number:").Check if the number is a mutliply of 5 or not.
+2)Given grade to student according to their score
+. 80-90,A
+. 70-79,B
+. 60-69,C
+. 50-59,D
+. 0-49,F
--- second.js
+++ second.js
@@ -1,13 +1,25 @@
 let number= prompt ("Enter the number:");
+if(number>=80 && number<=100)
 {
-if ( number%5===0){
-    console.log("Is mutiple of 5");
-    
+    console.log("Grade=A");
+}
+else if(number>=70 && number<=79)
+{
+    console.log("Grade=B");
+}
+else if(number>=60 && number<=69)
+{
+    console.log("Grade=c");
+}
+ else if(number>=50 && number<=59)
+{
+    console.log("Grade=D");
 }
 else{
-    console.log("Not mutliple of 5");
+    console.log("Garde=F");
 }
-}
+
+
 
 
 
