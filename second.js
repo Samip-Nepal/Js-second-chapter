e92bb67464121fec76f9f1ab84823e2932b294c7
Samip-Nepal <samipnepal5@email.com>
2024-02-04 09:42:23 +0545

    Ternary operator simple or shortcut of if else

--- second.js
+++ second.js
@@ -1,16 +1,5 @@
 //Condition statement
-let age=25;
-if(age<18){
-    console.log("Junior");
-}
-else if(age>60)
-{
-    console.log("Senior");
-}
-else 
-{
-    console.log("middle boy");
-}
+
 
 
 
